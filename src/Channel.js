02770796c39b48a40c7a66d1d258be3047787616
Iskandar Kurbanov <iskurbanov@gmail.com>
2019-07-24 14:49:14 -0400

    fixed number bug

--- src/Channel.js
+++ src/Channel.js
@@ -11,7 +11,7 @@ const Channel = ({value, handleValueChange}) => {
   return (
     <div className="channel">
       <button type="button" className="btn up" onClick={ () => updateValue(value + 1) } >+</button>
-      <input type="text" className="txt" value={value} onChange={ ({target}) => updateValue(target.value) } />
+      <input type="text" className="txt" value={value} onChange={ ({target}) => updateValue(Number(Number.isNaN(target.value) ? target.value : 0)) } />
       <button type="button" className="btn down" onClick={ () => updateValue(value -1 ) } >-</button>
     </div>
   )
